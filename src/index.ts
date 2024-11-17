dfa9d6f8430efaf02267b0d644631ca996dd2e31
k4ru3 <pzKARUzq2@gmail.com>
2024-11-17 22:09:31 +0900

    add upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,52 @@
-import exrpess, { Express } from 'express';
 import ws, { WebSocket } from 'ws';
 import http from 'http';
 
-const socketMap: Map<string, WebSocket[]> = new Map();
+type sdp = object|null;
 
-export function enablePeerModule(server: http.Server, { destroySocket: boolean } = { destroySocket: false }) {
+const socketMap: Map<string, { offer: sdp, answer: sdp, sockets: [WebSocket?, WebSocket?] }> = new Map();
+
+export function enablePeerModule(server: http.Server, path: string, option: { destroySocket: boolean } = { destroySocket: false }) {
 	const wss = new ws.Server({ noServer: true });
 	wss.on('connection', (ws: WebSocket) => {
 		ws.on('message', function (msg) {
+            const { type, id } = JSON.parse(msg.toString());
+            const pair = socketMap.get(id);
+            if(type === "join" && id !== "" && pair){
+                const joined = addSocket(ws, id);
+                if(joined){
+                    //参加成功
+
+                }
+            }
 		});
 	});
+
+    server.on('upgrade', (req, socket, head) => {
+        if(req.url === path){
+            wss.handleUpgrade(req, socket, head, (ws) => {
+                wss.emit('connection', ws, req);
+            })
+        }else if(option.destroySocket){
+            socket.destroy();
+        }
+    })
+}
+
+function addSocket(socket: WebSocket, id: string): boolean {
+    const pair = socketMap.get(id);
+    if(pair){
+        switch(pair.sockets.length){
+            case 1:
+                pair.sockets[0]?.send(JSON.stringify({
+                    type: 'reqest offer'
+                }))
+            case 0:
+                pair.sockets.push(socket);
+                return true;
+            default:
+                return false;
+        }
+    }
+    return false;
 }
 
